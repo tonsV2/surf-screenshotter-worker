71cf66e7f2884a33fd86466259981fdaac2c05cf
Sebastian Jensen <sebastianthegreatful@gmail.com>
2016-09-24 19:45:13 +0200

    Updated readme file. Both projects runs in docker. Fixed indentation in upload.php.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-sudo docker stop screenshotter && sudo docker rm screenshotter && sudo docker build -t surf/screenshotter . && sudo docker run --name screenshotter -d surf/screenshotter && sudo docker exec -i -t screenshotter bash
+
+sudo docker network create surf
+
+sudo docker stop screenshotter-worker && sudo docker rm screenshotter-worker && sudo docker build -t surf/screenshotter-worker . && sudo docker run --name screenshotter-worker --net=surf -d surf/screenshotter-worker && sudo docker exec -i -t screenshotter-worker bash
+
+sudo docker stop screenshotter-http && sudo docker rm screenshotter-http && sudo docker build -t surf/screenshotter-http . && sudo docker run --name screenshotter-http --net=surf -d -p 80:80 surf/screenshotter-http
+
--- www/upload.php
+++ www/upload.php
@@ -1,35 +1,35 @@
 <?php
 
-$target_dir = "uploads/";
-$target_file = "";
-$MAX_SIZE = 500000 * 2;
-// This needs to match the value found in the client script
-$SECRET_KEY="1a7adef0-9d3c-4fae-b1ea-10c3957fa1fb";
+	$target_dir = "uploads/";
+	$target_file = "";
+	$MAX_SIZE = 500000 * 2;
+	// This needs to match the value found in the client script
+	$SECRET_KEY="1a7adef0-9d3c-4fae-b1ea-10c3957fa1fb";
 
-if($_POST["secret_key"] != $SECRET_KEY) {
-	http_response_code(403);
-	exit();
-}
+	if($_POST["secret_key"] != $SECRET_KEY) {
+		http_response_code(403);
+		exit();
+	}
 
 // Confirm file isn't too big
-if(!$_POST["filename"]) {
-	http_response_code(400);
-	exit();
-} else {
-	$target_file = $target_dir . $_POST["filename"];
-}
+	if(!$_POST["filename"]) {
+		http_response_code(400);
+		exit();
+	} else {
+		$target_file = $target_dir . $_POST["filename"];
+	}
 
 // Delete file if it already exists
-if(file_exists($target_file)) {
-	unlink($target_file);
-}
+	if(file_exists($target_file)) {
+		unlink($target_file);
+	}
 
 // Move file from tmp/ to uploads/
-if(move_uploaded_file($_FILES["payload"]["tmp_name"], $target_file)) {
-	http_response_code(200);
-} else {
-	http_response_code(500);
-}
+	if(move_uploaded_file($_FILES["payload"]["tmp_name"], $target_file)) {
+		http_response_code(200);
+	} else {
+		http_response_code(500);
+	}
 
 ?>
 
