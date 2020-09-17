70834aaff466b0d4b91f9604567158df766c97ad
Sebastian Jensen <sebastianthegreatful@gmail.com>
2020-09-17 19:32:07 +0200

    Mv screenshots to /data/uploads

--- worker/screenshotter.sh
+++ worker/screenshotter.sh
@@ -6,13 +6,14 @@ PAGERES=/node_modules/.bin/pageres
 DATA_FILE=data_file.txt
 
 curl -L $DATA_URL -o $DATA_FILE
+mkdir -p /data/uploads
 
 while read line; do
   set -- $line
   cmd="$PAGERES $1 $2 --selector=$3 --filename=$4 --hide='$5' --cookie '$6' --timeout=20"
   echo $cmd
   eval $cmd
-  mv $4.png /data
+  mv $4.png /data/uploads
 done < $DATA_FILE
 
 rm $DATA_FILE
