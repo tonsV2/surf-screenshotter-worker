a1c329597697c1f2ab9ac14adffcde474a8b9f87
sj <sebastianthegreatful@gmail.com>
2020-10-01 12:35:37 +0700

    Return a summation of return codes.
    This is far from ideal but at least if a non-zero return code is returned in the loop the entire script will also return a non-zero return code.

--- scripts/screenshotter.sh
+++ scripts/screenshotter.sh
@@ -1,15 +1,20 @@
-#! /bin/sh
+#! /bin/bash
 
 DATA_FILE=/mnt/data/data_file.txt
 PAGERES=/node_modules/.bin/pageres
 
 mkdir -p /data/uploads
 
-while read line; do
+rc=0
+
+while read -r line; do
   set -- $line
   cmd="$PAGERES $1 $2 --selector=$3 --filename=$4 --hide='$5' --cookie '$6' --timeout=20"
   echo $cmd
   eval $cmd
-  mv $4.png /data/uploads
+  ((rc+=$?))
+  mv "$4.png" /data/uploads
+  ((rc+=$?))
 done < $DATA_FILE
 
+exit $rc
